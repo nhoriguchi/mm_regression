317baa67a06b05abcc2c61495a92009bc32681de
Naoya Horiguchi <n-horiguchi@ah.jp.nec.com>
2014-02-05 14:35:53 -0500

    add to sample project auto-downloading test_core

--- sample_test/run-test.sh
+++ sample_test/run-test.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-TESTCORE=/root/upstream/test_core/run-test.sh
+THISDIR=$(dirname $(readlink -f $BASH_SOURCE))
+[ ! -d ${THISDIR}/test_core ] && git clone https://github.com/Naoya-Horiguchi/test_core
+TESTCORE=${THISDIR}/test_core/run-test.sh
+
+[ ! -f ${TESTCORE} ] && echo "No test_core on ${THISDIR}/test_core." && exit 1
+
 TESTCASE_FILTER="$@"
 [ "$TESTCASE_FILTER" ] && TESTCASE_FILTER="-f \"${TESTCASE_FILTER}\""
 
