007f8134ee25e6f91f1a8318ad7d5707dd78ed4e
Naoya Horiguchi <n-horiguchi@ah.jp.nec.com>
2015-02-04 13:28:03 +0900

    fix check_and_define_tp

--- lib/common.sh
+++ lib/common.sh
@@ -2,9 +2,6 @@ MEMTOTAL=$(grep ^MemTotal: /proc/meminfo | awk '{print $2}')
 
 check_and_define_tp() {
     local symbol=$1
-    local name=$2
-    [ ! "$name" ] && name=$symbol
-    echo "$symbol=$TRDIR/$name"
-    eval $symbol=$TRDIR/$name
-    [ ! -x $(eval echo $"$symbol") ] && echo "$path not found." >&2 && exit 1
+    eval $symbol=$TRDIR/$symbol
+    [ ! -e $(eval echo $"$symbol") ] && echo "$symbol not found." >&2 && exit 1
 }
