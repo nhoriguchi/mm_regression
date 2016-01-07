0f5fe64aaa1b48527b8bcbf1de862f9625b4ad09
Naoya Horiguchi <n-horiguchi@ah.jp.nec.com>
2016-01-07 16:54:51 +0900

    run-test-new.sh support version output mode
    Signed-off-by: Naoya Horiguchi <n-horiguchi@ah.jp.nec.com>

--- run-test-new.sh
+++ run-test-new.sh
@@ -5,8 +5,9 @@ DEVEL_MODE=
 # RECIPEFILES might be set as an environment variable
 # RECIPEDIR might be set as an environment variable
 # TESTCASE_FILTER might be set as an environment variable
+SHOW_TEST_VERSION=
 
-while getopts vs:t:f:Spd:r:D OPT ; do
+while getopts vs:t:f:Spd:r:DV OPT ; do
     case $OPT in
         v) VERBOSE="-v" ;;
         s) KERNEL_SRC="${OPTARG}" ;;
@@ -17,10 +18,31 @@ while getopts vs:t:f:Spd:r:D OPT ; do
 		d) RECIPEDIR="$OPTARG" ;;
 		r) RECIPEFILES="$RECIPEFILES $OPTARG" ;;
 		D) DEVEL_MODE=true ;;
+		V) SHOW_TEST_VERSION=true ;;
     esac
 done
 
 shift $[OPTIND-1]
+
+export TCDIR=$(dirname $(readlink -f $BASH_SOURCE))
+# Assuming that current directory is the root directory of the current test.
+export TRDIR=$PWD
+
+. $TCDIR/setup_generic.sh
+. $TCDIR/setup_test_core.sh
+
+# record current revision of test suite and test_core tool
+if [ "$SHOW_TEST_VERSION" ] ; then
+	echo "Current test: $(basename $TRDIR)"
+	echo "TESTNAME/RUNNAME: $TESTNAME"
+	( cd $TRDIR ; echo "Test version: $(git log -n1 --pretty="format:%H %s")" )
+	( cd $TCDIR ; echo "Test Core version: $(git log -n1 --pretty="format:%H %s")" )
+	exit 0
+fi
+
+# keep backward compatibility with older version of test_core
+export NEWSTYLE=true
+
 [ "$RECIPEFILES" ] && RECIPEFILES="$(readlink -f $RECIPEFILES)"
 
 for rd in $RECIPEDIR ; do
@@ -34,27 +56,9 @@ done
 [ ! "$RECIPEFILES" ] && echo "RECIPEFILES not given or not exist." >&2 && exit 1
 export RECIPEFILES
 
-export TCDIR=$(dirname $(readlink -f $BASH_SOURCE))
-# Assuming that current directory is the root directory of the current test.
-export TRDIR=$PWD
-
-# keep backward compatibility with older version of test_core
-export NEWSTYLE=true
-
-. $TCDIR/setup_generic.sh
-. $TCDIR/setup_test_core.sh
 . $TCDIR/lib/recipe.sh
 . $TCDIR/lib/patch.sh
 . $TCDIR/lib/common.sh
-
-# record current revision of test suite and test_core tool
-echo "Current test: $(basename $TRDIR)"
-( cd $TRDIR ; echo "Test version: $(git log -n1 --pretty="format:%H %s")" )
-( cd $TCDIR ; echo "Test Core version: $(git log -n1 --pretty="format:%H %s")" )
-
-echo "TESTNAME/RUNNAME: $TESTNAME"
-echo "TESTCASE_FILTER: $TESTCASE_FILTER"
-echo "RECIPEFILES: ${RECIPEFILES//$TRDIR\/cases\//}"
 
 stop_test_running() {
 	ps x -o  "%p %r %y %x %c" | grep $$
@@ -63,8 +67,10 @@ stop_test_running() {
 
 trap stop_test_running SIGTERM
 
+echo "=====> start testing $(basename $TRDIR):$TESTNAME"
+echo "RECIPEFILES: ${RECIPEFILES//$TRDIR\/cases\//}"
+
 for recipe in $RECIPEFILES ; do
-	echo "===> recipe $recipe"
 	if [ ! -f "$recipe" ] ; then
 		"Recipe $recipe must be a regular file." >&2
 		continue
@@ -112,8 +118,8 @@ for recipe in $RECIPEFILES ; do
 		reset_per_testcase_counters
 		init_return_code
 
-		. .tmp.recipe
 		mv .tmp.recipe $TMPD/_recipe
+		. $TMPD/_recipe
 
 		if [ "$TEST_PROGRAM" ] ; then
 			do_test "$TEST_PROGRAM -p $PIPE $VERBOSE"
