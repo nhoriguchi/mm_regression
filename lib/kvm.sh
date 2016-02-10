092b7c5d87dabaeec11de040c480784cc2c11669
Naoya Horiguchi <n-horiguchi@ah.jp.nec.com>
2016-02-10 09:32:22 +0900

    add vm_start_wait_one
    Signed-off-by: Naoya Horiguchi <n-horiguchi@ah.jp.nec.com>

--- lib/kvm.sh
+++ lib/kvm.sh
@@ -77,11 +77,9 @@ vm_restart_wait() {
 	clear_vmdirty
 }
 
-vm_restart_wait_one() {
+vm_start_wait_one() {
 	local vm=$1
 
-	echo -n "Rebooting $vm ..."
-	virsh destroy $vm > /dev/null 2>&1
 	vm_start_wait $vm > /dev/null 2>&1
 	if [ $? -eq 0 ] ; then
 		echo "Rebooting done."
@@ -90,6 +88,14 @@ vm_restart_wait_one() {
 		echo "vm ($vm) failed to start."
 		return 1
 	fi
+}
+
+vm_restart_wait_one() {
+	local vm=$1
+
+	echo -n "Rebooting $vm ..."
+	virsh destroy $vm > /dev/null 2>&1
+	vm_start_wait_one $vm
 }
 
 vm_restart_if_unconnectable() {
