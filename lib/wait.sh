94c461ac8aed2506bdcdc2210594534cfde29a86
Naoya Horiguchi <n-horiguchi@ah.jp.nec.com>
2016-02-15 12:21:19 +0900

    add lib/wait.sh
    Signed-off-by: Naoya Horiguchi <n-horiguchi@ah.jp.nec.com>

---
+++ lib/wait.sh
@@ -0,0 +1,36 @@
+wait_timeout() {
+	local timeout=$1
+	shift 1
+	local pids="$@"
+
+	# wait $pids
+
+	echo "$pids"
+	while true ; do
+		# all processes are finished
+		if ! kill -0 $pids 2> /dev/null ; then
+			break
+		fi
+
+		if [ "$timeout" -le 0 ] ; then
+			kill -9 $pids 2> /dev/null
+			break
+		fi
+
+		sleep 1
+		timeout=$[timeout - 1]
+	done
+}
+
+if [[ "$0" =~ "$BASH_SOURCE" ]] ; then
+	pids=
+
+	sleep 2 &
+	pids="$pids $!"
+
+	sleep 8 &
+	pids="$pids $!"
+
+	wait_timeout 5 $pids
+fi
+
