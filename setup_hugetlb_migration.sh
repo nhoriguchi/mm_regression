0724f8ecd043cc8a09b501d3021743a840efea0e
Naoya Horiguchi <n-horiguchi@ah.jp.nec.com>
2016-10-27 18:29:14 +0900

    refactoring hotremove recipes
    Signed-off-by: Naoya Horiguchi <n-horiguchi@ah.jp.nec.com>

---
+++ setup_hugetlb_migration.sh
@@ -0,0 +1,2 @@
+. $TRDIR/setup_mmgeneric.sh
+. $TRDIR/setup_hugepage_migration.sh
--- setup_thp_migration.sh
+++ setup_thp_migration.sh
@@ -1,4 +1,5 @@
 . $TRDIR/setup_mmgeneric.sh
+. $TCDIR/setup_hugepage_migration.sh
 
 THP=true
 NUMA_NODE=2
@@ -9,84 +10,6 @@ _prepare() {
 
 _cleanup() {
 	cleanup_mmgeneric || return 1
-}
-
-check_migration_pagemap() {
-	local before=$1
-	local after=$2
-
-	diff -u1000000 $before $after > $TMPD/.mig.diff
-	local before=$(grep "^-" $TMPD/.mig.diff | wc -l)
-	local after=$(grep "^+" $TMPD/.mig.diff | wc -l)
-	local unchange=$(grep "^ " $TMPD/.mig.diff | wc -l)
-
-	echo_log "check pagemap"
-	if [ "$before" -gt 0 ] && [ "$after" -gt 0 ] ; then
-		if [ "$unchange" -ne 0 ] ; then
-			echo_log "some pages migrated ($unchange pages failed)"
-		else
-			echo_log "all pages migrated"
-		fi
-		return 0
-	else
-		echo_log "no page migrated"
-		return 1
-	fi
-}
-
-check_migration_hugeness() {
-	local before=$1
-	local after=$2
-
-	grep -e H_ -e _T $before | cut -f1,2 > $TMPD/.pagetypes.huge.1
-	grep -e H_ -e _T $after  | cut -f1,2 > $TMPD/.pagetypes.huge.2
-	diff -u1000000 $TMPD/.pagetypes.huge.1 $TMPD/.pagetypes.huge.2 > $TMPD/.pagetypes.huge.diff
-	local before=$(grep "^-" $TMPD/.pagetypes.huge.diff | wc -l)
-	local after=$(grep "^+" $TMPD/.pagetypes.huge.diff | wc -l)
-	local unchange=$(grep "^ " $TMPD/.pagetypes.huge.diff | wc -l)
-
-	echo_log "check hugepage migration"
-	if [ ! -s $TMPD/.pagetypes.huge.1 ] ; then
-		echo_log "no hugepage"
-		return 3
-	elif [ ! -s $TMPD/.pagetypes.huge.2 ] ; then
-		echo_log "hugepage disappeared (maybe split?)"
-		return 2
-	elif [ "$before" -gt 0 ] && [ "$after" -gt 0 ] ; then
-		if [ "$unchange" -ne 0 ] ; then
-			echo_log "some hugepages migrated ($unchange hugepages failed)"
-		else
-			echo_log "all hugepages migrated"
-		fi
-		return 0
-	else
-		echo_log "no hugepage migrated"
-		return 1
-	fi
-}
-
-# assume before/after migration data is taken by "get_mm_stats 1 $pid"
-# and "get_mm_stats 2 $pid"
-check_migration_done() {
-	local before=$1
-	local after=$2
-
-	if check_migration_pagemap $before $after ; then
-		set_return_code MIGRATION_PASSED
-	else
-		set_return_code MIGRATION_FAILED
-	fi
-	check_migration_hugeness $before $after
-	local ret=$?
-	if [ "$ret" == 0 ] ; then
-		set_return_code HUGEPAGE_MIGRATED
-	elif [ "$ret" == 1 ] ; then
-		set_return_code HUGEPAGE_NOT_MIGRATED
-	elif [ "$ret" == 2 ] ; then
-		set_return_code HUGEPAGE_DISAPPEARED
-	elif [ "$ret" == 3 ] ; then
-		set_return_code HUGEPAGE_NOT_EXIST
-	fi
 }
 
 _control() {
