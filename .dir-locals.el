e2f092115f3707a9500370aa32cbe5c3090654da
Naoya Horiguchi <n-horiguchi@ah.jp.nec.com>
2017-06-01 14:16:07 +0900

    add .dir-locals.el
    Signed-off-by: Naoya Horiguchi <n-horiguchi@ah.jp.nec.com>

---
+++ .dir-locals.el
@@ -0,0 +1,10 @@
+((nil . ((indent-tabs-mode . nil)
+         (tab-width . 2)
+         (fill-column . 80)))
+ (sh-mode . ((indent-tabs-mode . t)
+	     (sh-basic-offset . 4)
+	     (tab-width . 4)))
+ (c-mode . ((c-file-style . "Linux")))
+ ("src/imported"
+  . ((nil . ((change-log-default-name
+              . "ChangeLog.local"))))))
