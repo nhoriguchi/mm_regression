dfb548c1bc067cd7d84ff7e145383cf5a7431a77
Naoya Horiguchi <n-horiguchi@ah.jp.nec.com>
2018-10-22 17:00:23 +0900

    sample_test: use make prepare

--- sample_test/run.sh
+++ sample_test/run.sh
@@ -2,6 +2,6 @@ export RUNNAME=debug
 export AGAIN=true
 export RECIPEFILES="$(make allrecipes)"
 
-make update_recipes
+make prepare
 make test
 ruby test_core/lib/test_summary.rb -v -C work/$RUNNAME
