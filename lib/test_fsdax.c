3d4dadde493ced3e369ccb8a9561ce3628347b6d
Naoya Horiguchi <n-horiguchi@ah.jp.nec.com>
2018-12-25 12:49:57 +0900

    add pmem related-testcases
    Signed-off-by: Naoya Horiguchi <n-horiguchi@ah.jp.nec.com>

---
+++ lib/test_fsdax.c
@@ -0,0 +1,66 @@
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+
+#define MADV_HWPOISON   100
+
+#define MAP_SYNC        0x80000         /* perform synchronous page faults for the mapping */
+
+#define ALIGN(x, a)     (((x) + (a) - 1) & ~((a) - 1))
+#define PAGE_ALIGN(x)     ALIGN(x, 4096)
+
+#define ADDR_INPUT 0x700000000000
+
+int main(int argc, char **argv)
+{
+	int ret;
+	int i;
+	int fd;
+	char *array = malloc(4096);
+	char *addr;
+	int mmapflag = MAP_SHARED;
+	struct stat st;
+
+	i = stat("/mnt/pmem/ext4/data1", &st);
+	if (i == -1)
+		perror("stat");
+	printf("inode: %lx\n", st.st_ino);
+
+	fd = open("/mnt/pmem/ext4/date1", O_RDWR|O_CREAT);
+	/* fd = open("/mnt/pmem/ext4/data1", O_RDWR); */
+	memset(array, 'a', 4096);
+	pwrite(fd, array, 4096, 0);
+
+	i = fstat(fd, &st);
+	if (i == -1)
+		perror("fstat");
+	printf("inode: %lx\n", st.st_ino);
+
+	if (!strcmp(argv[1], "true")) {
+		printf("MAP_SYNC flag set.\n");
+		mmapflag |= MAP_SYNC;
+	}
+
+	addr = mmap((void *)ADDR_INPUT, 4096, PROT_READ|PROT_WRITE, mmapflag, fd, 0);
+	if (addr == (void *)MAP_FAILED) {
+		perror("mmap");
+		printf("Faield to mmap(), abort\n");
+		return 1;
+	}
+	printf("addr is %p\n", addr);
+
+	/* system("cat /proc/self/smaps"); */
+
+	for (i = 0; i < 4096; i++)
+		addr[i] = 'c';
+
+	system("cat /proc/self/smaps");
+	system("cat /proc/self/numa_maps");
+
+	return 0;
+}
