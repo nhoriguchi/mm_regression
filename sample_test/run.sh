b3de265880ecb5bfdac5b18a9e1a79970929a832
Naoya Horiguchi <n-horiguchi@ah.jp.nec.com>
2020-05-26 16:39:26 +0900

    adjust sample project to new recipe walking rule
    Signed-off-by: Naoya Horiguchi <n-horiguchi@ah.jp.nec.com>

--- sample_test/run.sh
+++ sample_test/run.sh
@@ -1,8 +1,15 @@
 export RUNNAME=debug
 export AGAIN=true
 
-make all
-make prepare
-[ ! "$RECIPEFILES" ] && export RECIPEFILES="$(make allrecipes)"
-make test
-ruby test_core/lib/test_summary.rb -v -P work/$RUNNAME
+export SOFT_RETRY=1
+export HARD_RETRY=1
+
+recipelist=$1
+
+if [ "$recipelist" ] ; then
+	export RECIPELIST=$recipelist
+fi
+
+make --no-print-directory prepare
+make --no-print-directory test
+ruby test_core/lib/test_summary.rb -v -C work/$RUNNAME
