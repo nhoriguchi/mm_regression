4d30f0078a5aef5fb7a83ba43998f98595ede491
Naoya Horiguchi <naoya.horiguchi@nec.com>
2020-06-09 16:44:38 +0900

    remove obsolete code in prepare script
    Signed-off-by: Naoya Horiguchi <naoya.horiguchi@nec.com>

--- lib/setup_result_directory.sh
+++ lib/setup_result_directory.sh
@@ -11,26 +11,4 @@ fi
 full_recipe=work/$RUNNAME/full_recipe_list
 
 mkdir -p work/$RUNNAME
-make --no-print-directory allrecipes | grep ^cases > work/$RUNNAME/full_recipe_list
-
-# disable recipe modification for a while
-exit
-
-for existing in $(find work/$RUNNAME -name '_recipe') ; do
-	existing_tc=$(dirname $existing | cut -f3- -d/)
-	if ! grep -qx cases/$existing_tc $full_recipe ; then
-		echo "### obsolete testcase $existing_tc"
-		if [ "$REMOVE_OBSOLETE_TEST_RESULTS" ] ; then
-			rm -rf $(dirname $existing)
-		else
-			echo "### set environment variable REMOVE_OBSOLETE_TEST_RESULTS if you want to remove result direcotries of obsolete testcases"
-		fi
-	fi
-done
-
-for rc in $(cat $full_recipe | cut -f2- -d/) ; do
-	if [ ! -d work/$RUNNAME/$rc ] ; then
-		mkdir -p work/$RUNNAME/$rc
-		parse_recipefile cases/$rc work/$RUNNAME/$rc/_recipe
-	fi
-done
+make --no-print-directory allrecipes | grep ^cases | sort > work/$RUNNAME/full_recipe_list
