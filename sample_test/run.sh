ec01dcbb96b1c927887d8ec6ec79824297c53121
Naoya Horiguchi <nao.horiguchi@gmail.com>
2020-06-26 01:19:26 +0900

    update sample_test/run.sh
    Signed-off-by: Naoya Horiguchi <nao.horiguchi@gmail.com>

--- sample_test/run.sh
+++ sample_test/run.sh
@@ -1,8 +1,27 @@
-export RUNNAME=debug
+[ ! "$RUNNAME" ] && RUNNAME=debug
+export RUNNAME
 # export AGAIN=true
 
 export SOFT_RETRY=1
 export HARD_RETRY=1
+
+if [[ "$1" =~ cases/ ]] ; then
+	if [ "$RUNNAME" == debug ] ; then
+		make prepare
+		grep $1 work/$RUNNAME/full_recipe_list > work/$RUNNAME/recipelist
+	else
+		make prepare
+		export FILTER="$1"
+	fi
+	if [ ! -s work/$RUNNAME/recipelist ] ; then
+		echo "no recipe matched to $1" >&2
+		exit 1
+	fi
+	make build
+	make --no-print-directory test
+	ruby test_core/lib/test_summary.rb work/$RUNNAME
+	exit 0
+fi
 
 recipelist=$1
 
@@ -10,6 +29,8 @@ if [ "$recipelist" ] ; then
 	export RECIPELIST=$recipelist
 fi
 
-make --no-print-directory prepare
+# make --no-print-directory prepare
+make build
+make prepare
 make --no-print-directory test
-ruby test_core/lib/test_summary.rb -v -C work/$RUNNAME
+ruby test_core/lib/test_summary.rb work/$RUNNAME
