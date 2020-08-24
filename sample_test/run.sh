6890b01ad20b8c43225724c823f0eabca52e7e48
Naoya Horiguchi <nao.horiguchi@gmail.com>
2020-08-24 15:49:32 +0900

    update run.sh in sample_test
    copied from run.sh of mm_regression.
    
    Signed-off-by: Naoya Horiguchi <nao.horiguchi@gmail.com>

--- sample_test/run.sh
+++ sample_test/run.sh
@@ -1,3 +1,8 @@
+cd $(dirname $BASH_SOURCE)
+
+[ ! "$TEST_DESCRIPTION" ] && TEST_DESCRIPTION="sample_test"
+export TEST_DESCRIPTION
+
 [ ! "$RUNNAME" ] && RUNNAME=debug
 export RUNNAME
 # export AGAIN=true
@@ -7,7 +12,17 @@ export SOFT_RETRY
 [ ! "$HARD_RETRY" ] && HARD_RETRY=1
 export HARD_RETRY
 
-if [[ "$1" =~ cases/ ]] ; then
+
+if [ "$FAILRETRY" ] ; then
+	make -s build
+	export RUNNAME=${FAILRETRY}.a
+	make prepare || exit 1
+	ruby test_core/lib/test_summary.rb -C work/$FAILRETRY | grep ^FAIL | cut -f4 -d' ' > work/$RUNNAME/recipelist
+	export BACKGROUND=true
+	# TODO: how to pass environment variables
+	make --no-print-directory test
+	exit 0
+elif [[ "$1" =~ cases/ ]] ; then
 	if [ "$RUNNAME" == debug ] ; then
 		make prepare
 		grep $1 work/$RUNNAME/full_recipe_list > work/$RUNNAME/recipelist
@@ -19,7 +34,7 @@ if [[ "$1" =~ cases/ ]] ; then
 		echo "no recipe matched to $1" >&2
 		exit 1
 	fi
-	make build
+	make -s build
 	make --no-print-directory test
 	exit 0
 fi
