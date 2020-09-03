bf68cf7ec5d003da02db17fd87dd9a9e4fe7a21c
Naoya Horiguchi <nao.horiguchi@gmail.com>
2020-09-03 15:11:50 +0900

    change basic_namespace_layout.sh to reset namespaces
    Signed-off-by: Naoya Horiguchi <nao.horiguchi@gmail.com>

--- lib/pmem/basic_namespace_layout.sh
+++ lib/pmem/basic_namespace_layout.sh
@@ -33,7 +33,7 @@ NR_DEVDAX=1
 NR_FSDAX=8
 NR_NAMESPACES=$[NR_SECTION + NR_RAW + NR_DEVDAX + NR_FSDAX]
 # align to 6 (interleave set)
-BASE_SIZE=$[(GB_REGIONS/NR_NAMESPACES/6)*6]
+BASE_SIZE=$[(GB_REGIONS/NR_NAMESPACES/8)*6]
 if [ "$BASE_SIZE" -gt 126 ] ; then
 	BASE_SIZE=126
 fi
@@ -53,29 +53,66 @@ for dev in $(cat $TMPDIR/destroy.blocks) ; do
 done
 ndctl destroy-namespace -f all || exit 1
 
+available_region() {
+	local size=$1
+
+	ndctl list -R | jq -r '.[] | select(.available_size > '$size') | .dev' | head -n1
+}
+
+# Note: ndctl-create-namespace might create unexpectedly multiple namespaces
+# with --force option, so we might need explicitly specify region with -r option.
+
 for i in $(seq $NR_SECTOR) ; do
 	mountpoint=/mnt/sector${i}
 	mkdir -p $mountpoint
-	ndctl create-namespace -m sector -s ${BASE_SIZE}G > $TMPDIR/create.sector${i} || continue
-	blockdev=$(jq -r ".blockdev" $TMPDIR/create.sector${i})
-	mkfs.$FSTYPE /dev/$blockdev || continue
+	region=$(available_region $[BASE_SIZE << 30])
+	if [ ! "$region" ] ; then
+		echo "no space available"
+		exit 1
+	fi
+	echo "ndctl create-namespace -f -r $region -n sector${i} -m sector -s ${BASE_SIZE}G"
+	ndctl create-namespace -f -r $region -n sector${i} -m sector -s ${BASE_SIZE}G > $TMPDIR/create.sector${i} || continue
+
+	blockdev=$(ndctl list -N -i | jq -r '.[] | select(.name == "sector'${i}'") | .blockdev' | head -1)
+	echo "mkfs.$FSTYPE -F /dev/$blockdev"
+	mkfs.$FSTYPE -F /dev/$blockdev || continue
 	mount /dev/$blockdev $mountpoint
 done
 
-for i in $(seq $NR_RAW) ; do # maybe unused
+for i in $(seq $NR_RAW) ; do
 	mkdir -p /mnt/raw$i
-	ndctl create-namespace -m raw -s ${BASE_SIZE}G > $TMPDIR/create.raw${i}
+	region=$(available_region $[BASE_SIZE << 30])
+	if [ ! "$region" ] ; then
+		echo "no space available"
+		exit 1
+	fi
+	echo "ndctl create-namespace -f -r $region -n raw${i} -m raw -s ${BASE_SIZE}G"
+	ndctl create-namespace -f -r $region -n raw${i} -m raw -s ${BASE_SIZE}G > $TMPDIR/create.raw${i}
 done
 
-for i in $(seq $NR_DEVDAX) ; do # maybe unused
-	ndctl create-namespace -m devdax -s ${BASE_SIZE}G > $TMPDIR/create.devdax${i}
+for i in $(seq $NR_DEVDAX) ; do
+	region=$(available_region $[BASE_SIZE << 30])
+	if [ ! "$region" ] ; then
+		echo "no space available"
+		exit 1
+	fi
+	echo "ndctl create-namespace -f -r $region -n devdax${i} -m devdax -s ${BASE_SIZE}G"
+	ndctl create-namespace -f -r $region -n devdax${i} -m devdax -s ${BASE_SIZE}G > $TMPDIR/create.devdax${i}
 done
 
 for i in $(seq $NR_FSDAX) ; do
 	mountpoint=/mnt/pmem${i}
 	mkdir -p $mountpoint
-	ndctl create-namespace -m fsdax -s ${BASE_SIZE}G > $TMPDIR/create.fsdax${i} || continue
-	blockdev=$(jq -r ".blockdev" $TMPDIR/create.fsdax${i})
-	mkfs.$FSTYPE /dev/$blockdev || continue
+	region=$(available_region $[BASE_SIZE << 30])
+	if [ ! "$region" ] ; then
+		echo "no space available"
+		exit 1
+	fi
+	echo "ndctl create-namespace -f -r $region -n fsdax${i} -m fsdax -s ${BASE_SIZE}G"
+	ndctl create-namespace -f -r $region -n fsdax${i} -m fsdax -s ${BASE_SIZE}G > $TMPDIR/create.fsdax${i} || continue
+
+	blockdev=$(ndctl list -N -i | jq -r '.[] | select(.name == "fsdax'${i}'") | .blockdev' | head -1)
+	echo "mkfs.$FSTYPE -F /dev/$blockdev"
+	mkfs.$FSTYPE -F /dev/$blockdev || continue
 	mount -o dax /dev/$blockdev $mountpoint
 done
