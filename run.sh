5378fec2be96efa5a549f3ffc5ff1d673f624578
Naoya Horiguchi <naoya.horiguchi@nec.com>
2022-05-31 11:26:57 +0900

    don't run WARNed testcase in later round

--- run.sh
+++ run.sh
@@ -116,7 +116,7 @@ run_test() {
 			make --no-print-directory prepare
 			if [ ! -f  work/$RUNNAME/recipelist ] ; then
 				if [ "$ROUND" -gt 1 ] ; then
-					ruby test_core/lib/test_summary.rb -C work/$BASERUNNAME/$[ROUND-1] | grep -e ^FAIL -e ^WARN | cut -f4 -d' ' > work/$RUNNAME/recipelist
+					ruby test_core/lib/test_summary.rb -C work/$BASERUNNAME/$[ROUND-1] | grep -e ^FAIL | cut -f4 -d' ' > work/$RUNNAME/recipelist
 				else
 					if [ -f work/$BASERUNNAME/recipelist ] ; then
 						cp work/$BASERUNNAME/recipelist work/$RUNNAME/recipelist
