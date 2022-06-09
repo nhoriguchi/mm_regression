435b2a73f52f2bc8f6227e638962a43798a6c4ec
Naoya Horiguchi <naoya.horiguchi@nec.com>
2022-06-09 11:55:08 +0900

    update lib/fragmentation.sh for proactive compaction

--- lib/fragmentation.sh
+++ lib/fragmentation.sh
@@ -7,119 +7,115 @@ cat <<EOF > /tmp/fragmentation.c
 #include <stdlib.h>
 #include <stdint.h>
 #include <sys/sysinfo.h>
-
 #include <signal.h>
 
 void sig_handle_flag(int signo) { ; }
 
-#define PM_ENTRY_BYTES		8
-#define PM_PFRAME_BITS		55
-#define PM_PFRAME_MASK		((1LL << PM_PFRAME_BITS) - 1)
-#define PM_PFRAME(x)		((x) & PM_PFRAME_MASK)
-#define PM_SOFT_DIRTY		(1ULL << 55)
-#define PM_MMAP_EXCLUSIVE	(1ULL << 56)
-#define PM_FILE			(1ULL << 61)
-#define PM_SWAP			(1ULL << 62)
-#define PM_PRESENT		(1ULL << 63)
-
 #define ADDR_INPUT	0x700000000000UL
 #define PS		4096
-// 1 回のバッチで pfn チェックする範囲は 2MB 分、512 ページ
-#define PM_BATCH	(1UL << 20)
-// 512 エントリが入るバッファサイズは 4kB
-#define PM_BUF		(PM_ENTRY_BYTES * PM_BATCH / PS)
+#define THPS		0x200000UL
 
-static unsigned long pagemap_pfn(uint64_t val) {
-        return (val & PM_PRESENT) ? PM_PFRAME(val) : 0;
+struct sysinfo si;
+int usage_control = 0;
+int free_control = 256; // [0, 256]
+int pipe_fd;
+
+int count_order_9() {
+	char buf[256];
+	FILE *fs;
+	int nr9, nr10;
+	int ret = -1;
+
+	fs = fopen("/proc/buddyinfo", "r");
+	if (!fs) {
+		perror("fopen");
+		exit(1);
+	}
+
+	while(fgets(buf, 256, fs)) {
+		char *ptr = strstr(buf, "Node 1, zone   Normal");
+
+		if (ptr) {
+			if (sscanf(ptr, "Node 1, zone   Normal %*d %*d %*d %*d %*d %*d %*d %*d %*d %d %d", &nr9, &nr10)) {
+				ret = nr9 + 2 * nr10;
+				break;
+			}
+		}
+	}
+	fclose(fs);
+
+	if (ret == -1) {
+		perror("failed to parse /proc/buddyinfo");
+		exit(1);
+	}
+
+	return ret;
+}
+
+char *mmap_thp(size_t size) {
+	char *p;
+
+	p = mmap((void *)ADDR_INPUT, size, PROT_READ|PROT_WRITE, MAP_ANONYMOUS|MAP_PRIVATE, -1, 0);
+	if (p == (void *)-1) {
+		perror("mmap");
+		exit(1);
+	}
+
+	if (madvise(p, size, MADV_HUGEPAGE) == -1) {
+		perror("madvise");
+		exit(1);
+	}
+
+	return p;
 }
 
 int main(int argc, char **argv) {
-	char *p;
-	char pagemap_path[512];
-	int pagemap_fd;
-	int pipe_fd;
 	int ret;
-	uint64_t *buf = (uint64_t *)malloc(PM_BUF);
-	int progress = 0;
-	int progress_total = 0;
-	unsigned long target;
-	unsigned long target_base;
-	struct sysinfo si;
+	char *p;
+	int thps;
+	int anons;
+	char *panon;
+	int i;
 
 	signal(SIGUSR1, sig_handle_flag);
-	signal(SIGBUS, sig_handle_flag);
 
-	sprintf(pagemap_path, "/proc/%d/pagemap", getpid());
-	pagemap_fd = open(pagemap_path, O_RDONLY);
-	if (pagemap_fd == -1) {
-		perror("open");
-		return 1;
-	}
+	if (argc > 1)
+		usage_control = strtol(argv[1], NULL, 0);
+	if (argc > 2)
+		free_control = strtol(argv[2], NULL, 0);
 
 	pipe_fd = open("/tmp/ffifo", O_RDWR);
 	if (pipe_fd == -1)
 		pipe_fd = 1;
 
-	target_base = 0;
-	for (int k = 0; k < 100; k++) {
-		int total_batches;
-		int start_pagemap_check = target_base / PM_BATCH;
+	sysinfo(&si);
+	p = mmap_thp(si.totalram);
+	thps = count_order_9() + usage_control;
 
-		sysinfo(&si);
-		target = si.freeram - (1UL << 28);
-		target = target - target % PM_BATCH;
-		dprintf(pipe_fd, "si.freeram %lx, target %lx\n", si.freeram, target);
-
-		p = mmap((void *)ADDR_INPUT + target_base, target, PROT_READ|PROT_WRITE,
-			 MAP_ANONYMOUS|MAP_SHARED, -1, 0);
-		if (p == (void *)-1) {
-			dprintf(pipe_fd, "target_base %lx, target %lx\n", target_base, target);
-			perror("mmap");
-			return 1;
-		}
-		ret = madvise(p, target, MADV_RANDOM);
-		if (ret == -1) {
-			perror("madvise");
-			return 1;
-		}
-		memset(p, 'a', target);
-
-		target_base += target;
-		progress_total = 0;
-		total_batches = target_base / PM_BATCH;
-
-		dprintf(pipe_fd, "Turn %d, %lx batches\n", k, total_batches);
-		if (argc > 1 && !strcmp(argv[1], "all"))
-			start_pagemap_check = 0;
-		for (int i = start_pagemap_check; i < total_batches; i++) {
-			ret = pread(pagemap_fd, buf, PM_BUF,
-				    PM_ENTRY_BYTES * (ADDR_INPUT + i * PM_BATCH) / PS);
-			if (ret != PM_BUF) {
-				perror("pread");
-				return 1;
-			}
-
-			progress = 0;
-			for (int j = 0; j < PM_BUF / PM_ENTRY_BYTES; j++) {
-				unsigned long pfn = pagemap_pfn(buf[j]);
-
-				if (pfn == 0)
-					continue;
-				if ((pfn % 2) == 1) {
-					ret = madvise((void *)ADDR_INPUT + i * PM_BATCH + j * 4096, 4096, MADV_REMOVE);
-					if (ret == -1) {
-						perror("madvise");
-						return 1;
-					}
-				} else {
-					progress++;
-				}
-			}
-			progress_total += progress;
-		}
-		dprintf(pipe_fd, "Turn %d, allocated %d\n", k, progress_total);
+	dprintf(pipe_fd, "usage_control %d, free_control %d\n", usage_control, free_control);
+	dprintf(pipe_fd, "allocate %d thps, then free %d/512 of them.\n", thps, free_control);
+	for (i = 0; i < thps; i++) {
+		char *ptr = p + i * THPS;
+		memset(ptr, 'a', THPS);
 	}
-	dprintf(pipe_fd, "keep memory allocated\n");
+
+	dprintf(pipe_fd, "thp allocated\n");
+	pause();
+
+	for (i = 0; i < thps; i++) {
+		char *ptr = p + i * THPS;
+
+		for (int j = 0; j < free_control; j++) {
+			// ret = madvise(ptr + (2 * j + 1) * PS, PS, MADV_DONTNEED);
+			ret = madvise(ptr + (2 * j + 1) * PS, PS, MADV_PAGEOUT);
+			// ret = madvise(ptr + (2 * j + 1) * PS, PS, MADV_REMOVE);
+			if (ret == -1) {
+				perror("madvise");
+				exit(1);
+			}
+		}
+	}
+	dprintf(pipe_fd, "fragmentation generated\n");
 	pause();
 }
 EOF
@@ -134,8 +130,11 @@ fragmentation_monitor() {
 /sys/kernel/debug/extfrag/extfrag_index
 /sys/kernel/debug/extfrag/unusable_index
 /proc/sys/vm/extfrag_threshold
-/proc/vmstat
 EOF
+	echo "### grep compact /proc/vmstat"
+	grep compact /proc/vmstat
+	echo "### free"
+	free
 }
 
 if [ "$1" == monitor ] ; then
@@ -147,6 +146,14 @@ PROACTIVNESS=20
 if [ "$1" ] && [[ "$1" =~ ^[0-9]*$ ]] ; then
 	PROACTIVNESS=$1
 fi
+USAGE_CONTROL=0
+if [ "$2" ] && [[ "$2" =~ ^[\-0-9]*$ ]] ; then
+	USAGE_CONTROL=$2
+fi
+FREE_CONTROL=256
+if [ "$3" ] && [[ "$3" =~ ^[0-9]*$ ]] ; then
+	FREE_CONTROL=$3
+fi
 
 gcc -o /tmp/fragmentation /tmp/fragmentation.c || exit 1
 
@@ -154,17 +161,18 @@ gcc -o /tmp/fragmentation /tmp/fragmentation.c || exit 1
 
 echo 1 > /proc/sys/vm/compact_memory
 echo 3 > /proc/sys/vm/drop_caches
-
-set -x
-echo never > /sys/kernel/mm/transparent_hugepage/defrag
+echo madvise > /sys/kernel/mm/transparent_hugepage/defrag
 echo 0 > /sys/kernel/mm/transparent_hugepage/khugepaged/defrag
-echo 0 > /proc/sys/vm/compact_unevictable_allowed
+echo 1 > /proc/sys/vm/compact_unevictable_allowed
 echo $PROACTIVNESS > /proc/sys/vm/compaction_proactiveness
-set +x
+echo 0 > /proc/sys/vm/compaction_proactiveness
+echo 60 > /proc/sys/vm/swappiness
 
-/tmp/fragmentation > /tmp/ffifo &
-pid=$?
-trap "kill $pid" SIGINT
+fragmentation_monitor > 0.log
+trace-cmd record -q -p function -l proactive_compact_node -l kcompactd_do_work -l compact_zone -T bash -c "numactl --membind 1 --cpunodebind 1 /tmp/fragmentation $USAGE_CONTROL $FREE_CONTROL" &
+sleep 3
+pid=$(pgrep -f -x "/tmp/fragmentation $USAGE_CONTROL $FREE_CONTROL")
+echo "pid:$pid"
 
 while true ; do
 	if ! kill -0 $pid 2> /dev/null ; then
@@ -173,7 +181,17 @@ while true ; do
 	elif read -t3 line <> /tmp/ffifo ; then
 		echo ">> $line"
 		case "$line" in
-			"keep memory allocated")
+			"thp allocated")
+				fragmentation_monitor > 1.log
+				echo 12 > /proc/sys/vm/compaction_proactiveness
+				sleep 1
+				kill -SIGUSR1 $pid
+				;;
+			"fragmentation generated")
+				sleep 1
+				fragmentation_monitor > 2.log
+				kill -SIGUSR1 $pid
+				sleep 3
 				break
 				;;
 			*)
@@ -181,6 +199,5 @@ while true ; do
 		esac
 	fi
 done
-
-echo "fragmentation is prepared."
-fragmentation_monitor
+sleep 1
+trace-cmd report
